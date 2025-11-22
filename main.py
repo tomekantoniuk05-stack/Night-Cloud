790c46b1294426b12bf6bfe731e3343a84781289
tomekantoniuk05-stack <tomek.antoniuk05@gmail.com>
2025-11-22 14:27:18 +0100

    Team Logo

--- main.py
+++ main.py
@@ -246,6 +246,6 @@ for name, btn in guziczkiZakladek.items():
         btn.config(command=onClose)
 
 #print(get_a_nc_file(dataTeraz.strftime("%m-%Y"), dataTeraz.day))
-#
+
 root.deiconify()
 root.mainloop()
