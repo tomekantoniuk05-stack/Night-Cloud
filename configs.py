66886aa6fb0f4ed16ced26df6ee89a8364a09bad
tomekantoniuk05-stack <tomek.antoniuk05@gmail.com>
2025-11-22 20:03:14 +0100

    configs

---
+++ configs.py
@@ -0,0 +1,3 @@
+DNI_TYGODNIA = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+TEXT_EDITOR_SIZES = [8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 48, 52, 56, 60, 64, 72, 80, 88, 99]
