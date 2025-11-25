4d6afa9fa92b2e4af333dfbacfd9c73978e63c31
tomekantoniuk05-stack <tomek.antoniuk05@gmail.com>
2025-11-25 22:40:37 +0100

    configs

--- configs.py
+++ configs.py
@@ -1,3 +1,5 @@
 DNI_TYGODNIA = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 TEXT_EDITOR_SIZES = [8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 48, 52, 56, 60, 64, 72, 80, 88, 99]
+
+NOT_SAVED_EXIT_TEXT = "The changes will be gone forever"
